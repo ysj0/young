d68f915a6b076c809c208066494bced367abaafa
crystal <sujeong-e@likelion.org>
2021-07-23 09:24:01 +0900

    0723-01

--- accountapp/views.py
+++ accountapp/views.py
@@ -10,6 +10,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.models import NewModel
+from accountapp.templates.accountapp.decoraters import account_ownership_required
 from accountapp.templates.accountapp.forms import AccountCreationForm
 
 
@@ -45,8 +46,11 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+
+has_ownership = [login_required,account_ownership_required]
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
@@ -54,9 +58,9 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
