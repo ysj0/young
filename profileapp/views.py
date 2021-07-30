55ab6ec6580f500fd1e885ac8bf90627156ecbd3
crystal <sujeong-e@likelion.org>
2021-07-30 09:16:40 +0900

    0730-01

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -15,7 +15,6 @@ from profileapp.models import Profile
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm  # 어떤 폼을 입력받아서 할 것인지
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
 
@@ -23,6 +22,8 @@ class ProfileCreateView(CreateView):
         form.instance.user = self.request.user  # 유저 특정해주기, 폼이 생성되면
         return super().form_valid(form)
 
+   def get_success_url(self):
+       return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
 
 @method_decorator(profile_ownership_required,'get')
 @method_decorator(profile_ownership_required,'post')
@@ -30,5 +31,7 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'target_profile'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
